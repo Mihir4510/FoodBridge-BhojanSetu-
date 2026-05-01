785727d3070549294174ff493c2f5ad17f95c88a
Mihir Parmar <mihirparmar766@gmail.com>
2026-05-01 17:22:33 +0530

    fix: dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,26 @@
+// require("dotenv").config();
+
+// const app = require("./src/app");
+// const connectdb = require("./src/config/db");
+
+// const http = require("http");
+// const { initSocket } = require("./src/config/socket");
+
+// // connect database
+// connectdb();
+
+
+
+// const server = http.createServer(app);
+
+// // initialize socket
+// initSocket(server);
+
+
+// // start server
+// server.listen(3000, () => {
+//     console.log(`Server Running on port 3000`);
+// });
 require("dotenv").config();
 
 const app = require("./src/app");
@@ -9,16 +32,14 @@ const { initSocket } = require("./src/config/socket");
 // connect database
 connectdb();
 
-
-
 const server = http.createServer(app);
 
 // initialize socket
 initSocket(server);
 
+const PORT = process.env.PORT || 3000;
 
 // start server
-server.listen(3000, () => {
-    console.log(`Server Running on port 3000`);
+server.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
 });
-
